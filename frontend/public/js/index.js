34a3c9490b337e051b49e40b802ca56f45eaaa3c
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-08-03 08:44:06 -0400

    load behavior

--- frontend/public/js/index.js
+++ frontend/public/js/index.js
@@ -1,21 +1,26 @@
-import { createLatinTextArea } from "./page_components/mainTextArea.js"; 
+import { createLatinTextArea } from "./page_components/mainTextArea.js";
 import { notesAreaCreate } from "./page_components/notesArea.js";
 import { sourceAreaCreate } from "./page_components/sourceArea.js";
+import { getDefinition } from "./page_components/fetchDefinition.js";
 
-
-const latinTextArea = document.getElementById('latinText');
+const latinTextArea = document.getElementById("latinText");
 // remove any highlights when mouse leaves the latin passage
-latinTextArea.addEventListener('mouseleave', () => {
-    document.querySelector(".word.highlight")?.classList.remove('highlight');
-})
+latinTextArea.addEventListener("mouseleave", () => {
+  document.querySelector(".word.highlight")?.classList.remove("highlight");
+});
 
 // build the page
 notesAreaCreate();
 sourceAreaCreate();
 createLatinTextArea();
 
-// set default value for source material 
-window.addEventListener("load", function() {
-    console.log("Page fully loaded");
-    this.document.querySelector("#sourceArea").value = "";
+// set default value for source material
+window.addEventListener("load", function () {
+  console.log("Page fully loaded");
+  const inputBox = this.document.querySelector("#sourceArea");
+  const textArea = this.document.querySelector("#enterText");
+  inputBox.value =
+    "Enter a word/paste source material to replace the passage below";
+  getDefinition("in");
+  textArea.focus();
 });
