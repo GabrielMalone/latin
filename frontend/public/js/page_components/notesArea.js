5ed8092e4d6e26b4a506dc45e7f5f94bc2929b09
GabrielMalone <167805738+GabrielMalone@users.noreply.github.com>
2024-08-07 00:18:46 -0400

    some fixes

--- frontend/public/js/page_components/notesArea.js
+++ frontend/public/js/page_components/notesArea.js
@@ -1,31 +1,16 @@
-const backgroundColor = "#0000000;"; // Ensure proper format
-
 const enterText = document.querySelector("#enterText");
-const notesContainer = document.querySelector("#notesContainer");
 
 /**
  * click to begin entering notes
  */
 export const notesAreaCreate = async () => {
-  enterText.innerHTML = startingNotesValue;
-
   enterText.addEventListener("click", () => {});
 
   enterText.addEventListener("mouseleave", () => {
-    if (enterText.innerHTML === "") {
-      enterText.innerHTML = startingNotesValue; // Use value if it's an input/textarea
-    }
-  });
-
-  notesContainer.addEventListener("click", () => {});
-
-  notesContainer.addEventListener("mouseleave", () => {
     enterText.style.color = "grey";
-    notesContainer.style.backgroundColor = backgroundColor;
   });
 
   enterText.addEventListener("mouseenter", () => {
     enterText.style.color = "antiquewhite";
-    notesContainer.style.backgroundColor = backgroundColor;
   });
 };
